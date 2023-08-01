52cd48207e3d88bf66737ff6894fe89ec1195d1b
Shubham-S-Nimje <shubhamsnimje@gmail.com>
2023-08-01 14:09:14 +0530

    be added to log in page

--- src/components/body/LoginForm.js
+++ src/components/body/LoginForm.js
@@ -1,23 +1,22 @@
-import React, { useRef } from 'react'
+import React, { useRef } from "react";
 import { useHistory } from "react-router-dom";
-import { useDispatch } from 'react-redux';
-import { authActions } from '../../store/index.js'
-import { useSelector } from 'react-redux';
+import { useDispatch } from "react-redux";
+import { authActions } from "../../store/index.js";
+import { useSelector } from "react-redux";
 
 const LoginForm = () => {
   const enteredemail = useRef();
   const enteredpass = useRef();
-  const history = useHistory()
-  const dispatch = useDispatch()
-  const isAuth = useSelector(state => state.auth.isAuthenticated)
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
 
   const OnSubmitHandler = (event) => {
     event.preventDefault();
     // console.log(enteredemail.current.value);
     // console.log(enteredpass.current.value);
 
-    const url =
-      "http://localhost:4000/login-user";
+    const url = "http://localhost:4000/login-user";
     fetch(url, {
       method: "POST",
       body: JSON.stringify({
@@ -34,68 +33,71 @@ const LoginForm = () => {
           return res.json();
         } else {
           return res.json().then((data) => {
-            let errorMessage = "Authentication failed";
+            // console.log(data)
+            let errorMessage = data.message;
             throw new Error(errorMessage);
           });
         }
       })
       .then((data) => {
-        console.log(data);
-        data && localStorage.setItem('localId',data.localId)
-        data && localStorage.setItem('idToken',data.idToken)
-        data && localStorage.setItem('email',data.email)
+        // console.log(data.user);
+        data && localStorage.setItem("localId", data.user.id);
+        data && localStorage.setItem("email", data.user.email);
         history.push("/Expense-Tracker-2/profile");
-        dispatch(authActions.login())
+        dispatch(authActions.login());
         console.log("User has successfully signed in");
       })
       .catch((err) => {
         alert(err.message);
       });
-
   };
 
   return (
     <>
-    {!isAuth && <div className="flex items-center justify-center px-12 py-2">
-      <form
-        className="border-2 m-8 p-2 bg-gray-300 rounded-md"
-        onSubmit={OnSubmitHandler}
-      >
-        <div className="text-left">
-          <div>
-            <label className="font-bold">Email address :</label>
-            <input
-              type="email"
-              required
-              ref={enteredemail}
-              className="w-full rounded-md border-2 p-2"
-              placeholder="Email address"
-            />
-          </div>
-          <div>
-            <label className="font-bold">Password :</label>
-            <input
-              type="password"
-              required
-              ref={enteredpass}
-              className="w-full rounded-md border-2 p-2"
-              placeholder="Password"
-            />
-          </div>
-        </div>
-
-        <div>
-          <button
-            type="submit"
-            className="bg-sky-600 min-w-full text-white py-2 my-2 rounded-md"
+      {!isAuth && (
+        <div className="flex items-center justify-center px-12 py-2">
+          <form
+            className="border-2 m-8 p-2 bg-gray-300 rounded-md"
+            onSubmit={OnSubmitHandler}
           >
-            Sign in
-          </button>
+            <div className="text-left">
+              <div>
+                <label className="font-bold">Email address :</label>
+                <input
+                  type="email"
+                  autoComplete="email"
+                  required
+                  ref={enteredemail}
+                  className="w-full rounded-md border-2 p-2"
+                  placeholder="Email address"
+                />
+              </div>
+              <div>
+                <label className="font-bold">Password :</label>
+                <input
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  ref={enteredpass}
+                  className="w-full rounded-md border-2 p-2"
+                  placeholder="Password"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="bg-sky-600 min-w-full text-white py-2 my-2 rounded-md"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
         </div>
-      </form>
-    </div>}
+      )}
     </>
   );
-}
+};
 
-export default LoginForm
+export default LoginForm;
